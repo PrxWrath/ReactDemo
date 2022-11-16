7925cb18cd0ee9688359e9b6f2011e630f6070a4
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2022-11-16 15:00:39 +0530

    Added first ExpenseItem

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function ExpenseItem() {
+  return (
+    <div>
+        <h3>Expense Items:</h3>
+        <li>Food Rs 10</li>
+        <li>Petrol Rs 100</li>
+        <li>Movies Rs 200</li>
+    </div>
+  )
+}
