c78de6ff217a02b7ddb575ef9abf1ed8e9ed2dc2
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2022-11-16 15:00:36 +0530

    Added first project demo files

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import ExpenseItem from './components/ExpenseItem';
+function App() {
+  return (
+    <div>
+      <header className="App-interface">
+        <h1>React Demo</h1>
+        <ExpenseItem/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
