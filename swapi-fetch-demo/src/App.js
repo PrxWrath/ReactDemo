d2a84f41468f387c5d5b98fefbf49db6c5c59b5e
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2022-12-14 20:01:03 +0530

    Added loader to swapi demo

--- swapi-fetch-demo/src/App.js
+++ swapi-fetch-demo/src/App.js
@@ -1,15 +1,20 @@
 import React, {useState} from 'react';
 
 import MoviesList from './components/MoviesList';
+import Loader from './components/Loader';
 import './App.css';
 
 function App() {
 
   const [movies, setMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const fetchMoviesHandler = async () => {
+    setIsLoading(true);
     try{
       let response = await fetch('https://swapi.dev/api/films');
       let movieJSON = await response.json();
+      setIsLoading(false);
       const movieData = movieJSON.results.map(movie=>{
         return(
           {
@@ -30,6 +35,7 @@ function App() {
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>
+        {isLoading&&<Loader/>}
         <MoviesList movies={movies} />
       </section>
     </React.Fragment>
