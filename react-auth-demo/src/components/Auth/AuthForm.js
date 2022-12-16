1926bc9b9e4a953e0ac6037e1cf321b0c88f0590
PrxWrath <90231861+PrxWrath@users.noreply.github.com>
2022-12-16 11:39:58 +0530

    Added Sign Up with Firebase Auth REST API

---
+++ react-auth-demo/src/components/Auth/AuthForm.js
@@ -0,0 +1,76 @@
+import { useState, useRef, useCallback } from 'react';
+
+import classes from './AuthForm.module.css';
+
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true);
+  const [waiting, setWaiting] = useState(false); 
+  const emailRef = useRef();
+  const passwordRef  = useRef();
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState);
+  };
+
+  const submitHandler = useCallback(async(e) => {
+    e.preventDefault();
+    if(isLogin){
+      //handle login
+    }
+    else{
+      setWaiting(true);
+      try{
+        const res = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCr27y7q1JW2qG764N0ZftdsCSWEVZcoVc',{
+          method:'POST',
+          body:JSON.stringify({
+            email:emailRef.current.value,
+            password: passwordRef.current.value,
+            returnSecureToken: true
+          }),
+          headers:{
+            'Content-Type':'application/json'
+          }
+        })
+        if(!res.ok){
+          const data = await res.json();
+          throw new Error(data.error.errors[0].message)
+        }
+
+        emailRef.current.value = '';
+        passwordRef.current.value = '';
+        
+      }catch(err){
+        alert(err.message);
+      }
+      setWaiting(false);
+    }
+  },[isLogin]);
+
+  return (
+    <section className={classes.auth}>
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor='password'>Your Password</label>
+          <input type='password' id='password' required ref={passwordRef}/>
+        </div>
+        <div className={classes.actions}>
+          {!waiting?<button>{isLogin ? 'Login' :'Create Account'}</button>:<button className={classes.toggle}>Sending Request...</button>}
+          <button
+            type='button'
+            className={classes.toggle}
+            onClick={switchAuthModeHandler}
+          >
+            {isLogin ? 'Create new account' : 'Login with existing account'}
+          </button>
+        </div>
+      </form>
+    </section>
+  );
+};
+
+export default AuthForm;
